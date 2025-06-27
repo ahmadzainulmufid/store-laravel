125056a05b495af9409cc36dfb6b56726d19acad
ahmadzainulmufid <ahmadzainulmufid27@gmail.com>
2025-06-27 22:10:52 +0700

    crud product

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,7 +30,8 @@
                         class="list-group-item list-group-item-action {{ request()->is('admin') ? 'active' : '' }}">
                         Dashboard
                     </a>
-                    <a href="#" class="list-group-item list-group-item-action">
+                    <a href="{{ route('admin.product.index') }}"
+                        class="list-group-item list-group-item-action {{ request()->is('admin/product*') ? 'active' : '' }}">
                         Products
                     </a>
                     <a href="{{ route('admin.category.index') }}"
